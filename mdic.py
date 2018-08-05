0f362b41a9a41e1f31ed84846dda546a30d46807
Meghdad Shamsaei <meghdad@shamsaei.com>
2018-08-05 18:05:26 +0430

    Update mdic.py

--- mdic.py
+++ mdic.py
@@ -1,100 +1,21 @@
-from readmdict import MDX
+#!/bin/python
+
+from readmdict import MDX#or MDD  #You need to install lzo Library for gcc and python-lzo,readmdict,ripemd128 and pureSalsa20 Library for python
 import csv
 
-csvaddress='ar.csv'
+csvaddress='XXXX.csv' #Your CSV file name to export
 
-
-
-
-mdx= MDX('ar.mdx')
+mdx= MDX('XXXX.mdx') #Open the original MDX Dictionary file
 items = mdx.items()
-tx=mdx.items().next()
+tx=mdx.items().next() #To fetch first line in your mdx file - If delete this line you miss first line
 lst=[]
-i=0
+
 for counter in items:
     try:
         templst=list(tx)
         lst.append(templst)
         tx=items.next()
     except:
-        length=len(lst)
-        #for count in xrange(20):
         with open(csvaddress,'w') as output:
             writer = csv.writer(output, lineterminator='\n')
             writer.writerows(lst)
-
-
-
-
-
-
-
-
-"""
-            string=str(lst)
-            print(string.encode('utf-8'))
-            #print(str(lst[count][1]).encode('utf-8'))
-"""
-
-"""
-        for i in range(len(lst2)):
-            print (i)
-            print(type(lst2[i]))
-"""
-
-
-
-
-
-"""
-file = open('fulldic2.html', "w")
-for i in items:
-    file.write(str(items.next())+'\n')
-file.close()
-"""
-"""
-f = codecs.open('mdic.html', encoding = 'utf')
-for line in f:
-    print(repr(line).decode())
-"""
-#result = [tuple(map(lambda x: x.encode('utf-16'), tup)) for tup in mytx]
-#print(tx)
-
-"""
-with io.open("mdic.html" , "r" , encoding='utf-8') as mdic_file:
-    my_unicode_str = mdic_file.read()
-print(my_unicode_str)
-"""
-
-
-#print(mytx.decode('utf-8')))
-
-#print(tx.decode('utf-8'))
-
-#result = [tuple(map(lambda x: x.encode('utf-16'), tup)) for tup in tx]
-
-"""
-text= tx
-print(text.decode('utf-8'))
-
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-
-print(items.next())
-"""
