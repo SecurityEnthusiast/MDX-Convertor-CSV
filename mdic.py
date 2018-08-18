1b7e1e299a41465e3a09f431a37d23f55bf4cbb1
Meghdad Shamsaei <meghdadshamsaei@Meghdads-MacBook-Pro.local>
2018-08-18 18:04:07 +0430

    With Tag Remover Function

--- mdic.py
+++ mdic.py
@@ -1,9 +1,16 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 
 from readmdict import MDX#or MDD  #You need to install lzo Library for gcc and python-lzo,readmdict,ripemd128 and pureSalsa20 Library for python
 import csv
+import re
 
 csvaddress='XXXX.csv' #Your CSV file name to export
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
 
 mdx= MDX('XXXX.mdx') #Open the original MDX Dictionary file
 items = mdx.items()
@@ -13,6 +20,7 @@ lst=[]
 for counter in items:
     try:
         templst=list(tx)
+        templst[1] = remove_html_tags(templst[1])
         lst.append(templst)
         tx=items.next()
     except:
